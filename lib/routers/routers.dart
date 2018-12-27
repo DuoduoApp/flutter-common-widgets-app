316779db2ea48b39a1f6576d626b758d4fbe0d2d
zhu.yan <zhu.yan@alibaba-inc.com>
2018-12-27 16:33:22 +0800

    Update: add NavigationToolbar comps

--- lib/routers/routers.dart
+++ lib/routers/routers.dart
@@ -24,6 +24,7 @@ class Routes {
           handlerFunc: (BuildContext context, Map<String, List<String>> params) {
             return demo.buildRouter(context);
       });
+      print('路由:${demo.routerName}');
       router.define('${demo.routerName}', handler: handler);
     });
   }
