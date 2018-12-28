cfb3ced736bd997deb114f374f93863fe017db11
yifeng.yl <yifeng.yl@alibaba-inc.com>
2018-12-28 15:06:13 +0800

    feat:Theme

--- lib/widgets/themes/Material/index.dart
+++ lib/widgets/themes/Material/index.dart
@@ -2,6 +2,9 @@ import "package:flutter/material.dart";
 import '../../../model/widget.dart';
 
 import 'MaterialPageRoute/index.dart' as MaterialPageRoute;
+import 'MaterialApp/index.dart' as MaterialApp;
+import 'MaterialButton/index.dart' as MaterialButton;
+import 'MaterialColor/index.dart' as MaterialColor;
 
 
 List<WidgetPoint> widgetPoints = [
@@ -9,6 +12,21 @@ List<WidgetPoint> widgetPoints = [
     name: 'MaterialPageRoute',
     routerName: MaterialPageRoute.Demo.routeName,
     buildRouter: (BuildContext context) => MaterialPageRoute.Demo(),
+  ),
+  WidgetPoint(
+    name: 'MaterialApp',
+    routerName: MaterialApp.Demo.routeName,
+    buildRouter: (BuildContext context) => MaterialApp.Demo(),
+  ),
+  WidgetPoint(
+    name: 'MaterialColor',
+    routerName: MaterialColor.Demo.routeName,
+    buildRouter: (BuildContext context) => MaterialColor.Demo(),
+  ),
+  WidgetPoint(
+    name: 'MaterialButton',
+    routerName: MaterialButton.Demo.routeName,
+    buildRouter: (BuildContext context) => MaterialButton.Demo(),
   ),
   
 
