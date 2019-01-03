16cbda13e6768806b153c608891d1de7b4174204
三露 <xiaojia.dxj@alibaba-inc.com>
2019-01-03 15:42:00 +0800

    feat

--- lib/widgets/themes/Material/MaterialPageRoute/index.dart
+++ lib/widgets/themes/Material/MaterialPageRoute/index.dart
@@ -4,9 +4,6 @@ import 'package:flutter_rookie_book/common/widget_demo.dart';
 
 const Text0 = '''
 ### **简介**
-> 用来定义单一的强调色，以及四种色调的色系
-- 颜色的阴影用索引表示，索引较小的颜色比较浅，较大的索引比较暗。
-- 有个四个有效索引，100，200，400，和700.此颜色的值应与索引200和shade200的值相同。
 ''';
 
 const String Text1 = """
